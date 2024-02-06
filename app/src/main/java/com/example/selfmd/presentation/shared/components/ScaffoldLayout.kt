44e9bed6651c5cb7b75295d0ffc8c91e4f658c86
taha-cmyk <taha.programmer@gmail.com>
2024-02-06 12:36:31 +0100

    init repo

--- app/src/main/java/com/example/selfmd/presentation/shared/components/ScaffoldLayout.kt
+++ app/src/main/java/com/example/selfmd/presentation/shared/components/ScaffoldLayout.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 
